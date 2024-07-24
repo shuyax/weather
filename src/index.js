6b33b7f7d70a7cd29f70c065045574da747831dd
Shuya <macmini@macs-mbp.mynetworksettings.com>
2024-07-24 14:01:53 -0400

    test webhook

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ function unitSelectionEvent(current_temperature_c,current_temperature_f,conditio
     })
 }
 
+
 async function getWeather(location){
     if (location == null) {
         location = 'Pittsburgh'
