0615ccd3a687484c3c25688b3f87ba6aeba19d81
Shuya <macmini@macs-mbp.mynetworksettings.com>
2024-08-02 11:50:30 -0400

    Update the Flask appilication URL

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ async function getWeather(location){
         location = 'Pittsburgh'
     }
     const header = new Headers();
-    const request = new Request(`https://www.shuyaxu.com/api?location=${location}`,{
+    const request = new Request(`https://www.shuyaxu.com/weather/api?location=${location}`,{
         method: 'GET',
         headers: header,
         mode: 'cors'
